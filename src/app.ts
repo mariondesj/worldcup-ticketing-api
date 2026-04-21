d4248f3b989fefc211de0d932d20d2f0efde7339
User <user@MacBook-Air-de-moi.local>
2026-04-21 10:36:04 +0200

    add env example

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,17 @@ import { Hono } from 'hono'
 
 export const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/', (c) => // route principale (entrée)
+  //par default : return c.text('Hello Hono!') 
+  c.json({
+    success : true,
+    message : "World Cup Ticketing API"
+  })
+)
+app.get('/health', (c) =>// route health
+  c.json({
+    status : "ok"
+  })
+)
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { app } from './app'
+import { app } from './app'//👉 Tu importes ton application
 
 export default {
-  port: 3000,
-  fetch: app.fetch,
+  port: 3000,//Le serveur va tourner sur :http://localhost:3000
+  fetch: app.fetch,//Ça relie ton serveur à ton app
 }
